581102cd5217ee7d7b20f2ba1053e05f5a579f88
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-08-30 01:07:31 +0900

    トランザクションのinputとoutputの型を定義

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -32,9 +32,26 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	err = fn(q)
 	if err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
-			return fmt.Errorf("tx err: %v,rb err: %v",err,rbErr)
+			return fmt.Errorf("tx err: %v,rb err: %v", err, rbErr)
 		}
 		return err
 	}
 	return tx.Commit()
 }
+
+//TransferTxParams contains the input parameters of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+//TransferTxResult is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+}
+
+// TransactionTx performs a money transfer from one account to the other
+// It create a transfer record,add account entries,and update accounts' balance within asingle database transaction
