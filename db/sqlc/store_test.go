51151a88d5d3affd1ed38a3a7e6823f69d789ebf
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-09-01 16:16:25 +0900

    fmt削除

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,7 +13,6 @@ func TestTransferTx(t *testing.T) {
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
-	fmt.Println(">> before",account1.Balance,account2.Balance)
 	// run n concurrent transfer transactions
 	n := 5
 	amount := int64(10)
@@ -24,10 +22,8 @@ func TestTransferTx(t *testing.T) {
 
 	// run n concurrent transfer transaction
 	for i := 0; i < n; i++ {
-		txName := fmt.Sprintf("tx %d",i+1)
 		go func() {
-			ctx := context.WithValue(context.Background(),txKey,txName)
-			result, err := store.TransferTx(ctx, TransferTxParams{
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
@@ -87,7 +83,6 @@ func TestTransferTx(t *testing.T) {
 		require.Equal(t, account2.ID, toAccount.ID)
 
 		// TODO:check accounts'balance　TDD
-		fmt.Println(">> tx",fromAccount.Balance,toAccount.Balance)
 		diff1 := account1.Balance - fromAccount.Balance
 		diff2 := toAccount.Balance - account2.Balance
 		require.Equal(t, diff1, diff2)
@@ -105,7 +100,6 @@ func TestTransferTx(t *testing.T) {
 	updatedAccount2 ,err := testQueries.GetAccount(context.Background(),account2.ID)
 	require.NoError(t,err)
 
-	fmt.Println(">> after",account1.Balance,account2.Balance)
 	require.Equal(t,account1.Balance-int64(n)*amount,updatedAccount1.Balance)
 	require.Equal(t,account2.Balance+int64(n)*amount,updatedAccount2.Balance)
 
