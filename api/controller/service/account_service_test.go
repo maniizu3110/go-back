63d80a1a03dfd8211ef3ba9083e19e817198d115
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-09-07 01:17:26 +0900

    mockテストpassした

--- api/controller/service/account_service_test.go
+++ api/controller/service/account_service_test.go
@@ -17,46 +17,74 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-
-func TestGetAccount(t *testing.T){
+func TestGetAccount(t *testing.T) {
 	account := randomAccount()
-	ctrl := gomock.NewController(t)
-	//これをNew関数に渡せばうまくserviceの形にできそう
-	store := mockdb.NewMockStore(ctrl)
-	// build stubs
-	store.EXPECT().GetAccount(gomock.Any(),gomock.Eq(account.ID)).Times(1).Return(account,nil)
+	testCases := []struct {
+		name          string
+		accountID     int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name:      "OK",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccount(t, recorder.Body, account)
+			},
+		},
+		//TODO:add more cases
+	}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			//これをNew関数に渡せばうまくserviceの形にできそう
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+			// build stubs
+			// store.EXPECT().GetAccount(gomock.Any(),gomock.Eq(account.ID)).Times(1).Return(account,nil)
 
-	server := api.NewServer(store)
-	recoder := httptest.NewRecorder()
+			server := api.NewServer(store)
+			recoder := httptest.NewRecorder()
 
-	url := fmt.Sprintf("/api/v1/account/%d",account.ID)
-	fmt.Println(url)
-	request,err := http.NewRequest(http.MethodGet,url,nil)
-	require.NoError(t,err)
+			url := fmt.Sprintf("/api/v1/account/%d", account.ID)
+			fmt.Println(url)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
 
-	server.SetRouter().ServeHTTP(recoder,request)
-	//check response
-	require.Equal(t,http.StatusOK,recoder.Code)
-	requireBodyMatchAccount(t,recoder.Body,account)
+			server.SetRouter().ServeHTTP(recoder, request)
+			tc.checkResponse(t,recoder)
+			//check response
+			// require.Equal(t,http.StatusOK,recoder.Code)
+			// requireBodyMatchAccount(t,recoder.Body,account)
+
+		})
+	}
 
 }
 
-
-func randomAccount() sqlc.Account{
+func randomAccount() sqlc.Account {
 	return sqlc.Account{
-		ID: util.RandomInt(1,1000),
-		Owner: util.RandomOwner(),
-		Balance: util.RandomMoney(),
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
 }
 
-func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account sqlc.Account){
-	data,err := ioutil.ReadAll(body)
-	require.NoError(t,err)
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account sqlc.Account) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
 
 	var gotAccount sqlc.Account
-	err = json.Unmarshal(data,&gotAccount)
-	require.NoError(t,err)
-	require.Equal(t,account,gotAccount)
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, account, gotAccount)
 }
